77201c915083f6076c7e33ed8e027524d402b40e
assokk <mijisuu@naver.com>
2025-08-20 14:01:11 +0900

    refactor: 엔티티에 필드 및 연관관계 추가

--- src/main/java/org/example/newsfeed/comment/entity/Comment.java
+++ src/main/java/org/example/newsfeed/comment/entity/Comment.java
@@ -1,11 +1,13 @@
 package org.example.newsfeed.comment.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.newsfeed.feed.entity.Feed;
+import org.example.newsfeed.user.entity.User;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -15,4 +17,21 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "postId", nullable = false)
+    private Feed feed;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt =  LocalDateTime.now();
+
+    @Column
+    private LocalDateTime modifiedAt;
 }
