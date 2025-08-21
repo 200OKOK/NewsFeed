4bca9498ea20908a999c11c21508c5410ca73aa6
csy10001 <fuitshine@gmail.com>
2025-08-21 11:50:54 +0900

    fix : User 엔티티에서  Base엔티티 extend

--- src/main/java/org/example/newsfeed/user/dto/UserDeleteRequest.java
+++ src/main/java/org/example/newsfeed/user/dto/UserDeleteRequest.java
@@ -1,11 +1,16 @@
 package org.example.newsfeed.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserDeleteRequest {
+
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String userId;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
