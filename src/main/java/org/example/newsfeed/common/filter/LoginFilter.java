6291d03bfa1166d15997297602f78dd95c4b0763
dxxwls <eunjin0468@naver.com>
2025-08-22 20:17:04 +0900

    feat(filter): 로그인 검증 필터 JSON 응답으로 변경

--- src/main/java/org/example/newsfeed/common/filter/LoginFilter.java
+++ src/main/java/org/example/newsfeed/common/filter/LoginFilter.java
@@ -27,12 +27,9 @@ public class LoginFilter implements Filter {
         if (!isWhiteList(requestURI)) {
             HttpSession session = httpRequest.getSession(false);
             if (session == null || session.getAttribute("로그인 유저") == null) {
-                throw new RuntimeException("로그인 해주세요.");
                 ObjectMapper mapper = new ObjectMapper();
                 response.setContentType("application/json;charset=UTF-8");
                 response.getWriter().write(mapper.writeValueAsString(new ErrorResponse("USR-009", "로그인이 필요합니다.", httpRequest.getRequestURI())));
-                return;
-                httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인 해주세요.");
                 return;
             }
         }
