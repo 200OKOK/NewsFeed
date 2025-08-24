c94d6dafe9b517b1e0ac5b53191d8cb726c0a881
csy10001 <fuitshine@gmail.com>
2025-08-24 16:52:35 +0900

    fix : dto에서 setter를 뺐습니다.

--- src/main/java/org/example/newsfeed/user/dto/UserLoginRequest.java
+++ src/main/java/org/example/newsfeed/user/dto/UserLoginRequest.java
@@ -1,10 +1,8 @@
 package org.example.newsfeed.user.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class UserLoginRequest {
     private String userId;
     private String password;
